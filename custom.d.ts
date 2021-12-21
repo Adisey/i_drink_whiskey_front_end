0ec106e36615b9b0010c37e93e7c56bc56b63485
Adisey <iadisey@gmail.com>
2021-12-21 15:56:26 +0200

    - add typing *.graphql

--- custom.d.ts
+++ custom.d.ts
@@ -2,5 +2,5 @@ declare module "*.svg" {
   const context: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default context;
 }
-
 declare module "*.module.scss";
+declare module "*.graphql";
