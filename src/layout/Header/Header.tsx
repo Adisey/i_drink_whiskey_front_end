094a036673f72eb0e3609c4c63354a00b8e228f0
Adisey <iadisey@gmail.com>
2022-01-28 16:26:52 +0200

    - test docker update

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -13,8 +13,8 @@ export const Header: React.FC<IDivMainProps> = ({
 }: IDivMainProps): JSX.Element => {
   return (
     <div {...props} className={cx(Styles.main, className)}>
-      <div className={Styles.titlePlace}>Title</div>
       <div className={Styles.logoPlace}>Logo</div>
+      <div className={Styles.titlePlace}>Title</div>
       <div className={Styles.rightPlace}>
         <HeaderUserInfo className={Styles.userInfo} />
         <div className={Styles.mobileMenu}>Mobile Menu</div>
